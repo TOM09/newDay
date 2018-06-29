f9132a584974ab958ee5bd4865ce76ee12e1c5aa
TOM09 <songhua0209@foxmail.com>
2018-06-29 10:50:47 +0800

    blog

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Params from '@/components/params'
-import Hi1 from '@/components/Hi1'
-import error from '@/components/error'
+import Count from '@/components/count'
+
 
 
 Vue.use(Router)
@@ -16,21 +15,8 @@ export default new Router({
       name: 'Hello',
       component:HelloWorld
     },{
-      path: '/params/:newsId/:newsTitle',
-      component:Params
-    },{
-      path: '/gohome',
-      redirect:'/'
-    },{
-      path: '/Hi1',
-      component:Hi1
-    },{
-      path: '/Hi1',
-      component:Hi1,
-      alias:'/goHi221'
-    },{
-      path: '*',
-      component:error
+      path: '/count',
+      component:Count
     }
   ]
 })
